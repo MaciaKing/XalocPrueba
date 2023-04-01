a371e63bf2eac28efc1eced74d4790c6480dd8fe
MaciaKing <maciasalvasalva@gmail.com>
2023-04-01 13:50:53 +0200

    Shooting

--- EntrevistaXaloc/Assets/Scripts/SingelPlayerGame.cs
+++ EntrevistaXaloc/Assets/Scripts/SingelPlayerGame.cs
@@ -14,6 +14,12 @@ public class SingelPlayerGame : MonoBehaviour{
     private int maxBullets = 15;
     private int maxShield = 1;
 
+    public GameObject bullet;
+    public Transform spawnBullet; 
+    public float shotForce = 15000f;
+    public float shotRate = 0.5f;
+    public float shoteRateTime = 0f;
+
     void Start(){
         // spawn cactus
         for (int i = 0; i < 4; i++){
@@ -22,6 +28,11 @@ public class SingelPlayerGame : MonoBehaviour{
     }
 
     void Update(){
+        // shot
+        if (Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space pressed DISPAAAROOOOO");
+            shot();
+        }
         //update bullets and shield
         canvasManager.setBullets(bullets.ToString());
         canvasManager.setShield(shield.ToString());
@@ -51,5 +62,22 @@ public class SingelPlayerGame : MonoBehaviour{
         Vector3 pos = new Vector3(x, 0f, z);
         GameObject newCactus = Instantiate(cactus, pos, Quaternion.identity);
     }
+    
+    private void shot(){
+        if (bullets > 0){
+            if(Time.time > shoteRateTime){
+                bullets--;
+                //GameObject newBullet = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+                //newBullet.GetComponent<Rigidbody>().AddForce(spawnBullet.forward * shotForce);
+                GameObject newBullet = Instantiate(bullet, spawnBullet.position, spawnBullet.rotation);
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnBullet.forward * shotForce);
+                Debug.DrawRay(spawnBullet.position, spawnBullet.forward * 10f, Color.black, 4f);
 
+                shoteRateTime = Time.time + shotRate;
+                Destroy(newBullet, 2f);
+            }
+           
+            
+        }
+    }
 }
